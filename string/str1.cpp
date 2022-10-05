be984f9aabf0978120af248a6b6080756dfa85e8
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-10-05 07:49:20 +0530

    Create str1.cpp

---
+++ string/str1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char a[]={'1','f'};
+    char b[]="Ad";
+    cout<<strlen(a)<<endl;
+    cout<<strlen(b)<<endl;
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b);
+
+    return 0;
+}
+
+output is :
+4
+2
+2
+3
+ 
