4b424d3d582f10d0ba41f4c6cb7d7d2faa9425f8
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-10-08 01:33:40 +0530

    Create cls_put_get_datails.cpp

---
+++ c++/Class/constructor/cls_put_get_datails.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+
+using namespace std;
+class A{
+    public:
+    int b;
+     void p(int i){
+         int b=i;
+     }
+     void g(){
+        //  cout<<"enter"<<b<<endl;
+        //  cin>>b;
+         cout<<"value of b : "<<b<<endl;
+         
+     }
+    
+};
+
+int main()
+{
+    A s1;
+    
+    int j;
+    cout<<"enter "<<endl;
+    cin>>j;
+    
+    s1.p(j);
+    s1.g();
+    return 0;
+}
+
+output of the program:
+
+enter 
+7
+value of b : 1359180256
