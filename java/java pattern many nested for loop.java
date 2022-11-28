3b98b9c4154595db6f50412448ae675b325141aa
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-11-28 17:44:28 +0530

    Create java pattern many nested for loop.java
    2
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    * 
    
    * 
    *

---
+++ java/java pattern many nested for loop.java
@@ -0,0 +1,89 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		
+		
+		for(int i=0; i<n; i++){
+		    for(int j=0; j<n; j++){
+		        for(int k=0; k<n; k++){
+		            for(int l=0; l<n; l++){
+		               for(int m=0; m<n; m++){
+		            System.out.print("* ");
+		    System.out.println();
+		                }
+		                System.out.println();
+		            }
+		        }
+		    }
+		}
+		
+		
+	}
+}
+
+
+
+
+output of the program::
+
+2
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
+
+* 
+* 
