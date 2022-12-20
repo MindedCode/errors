0a6cc1b19b673f257d56625e565af860f0c00a73
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-12-20 10:12:09 +0530

    Create allString.java

---
+++ allString.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main{
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String name = "Minded";
+        System.out.println("Your name is : " + name);
+        
+        String fullName = "Minded Code";
+        System.out.println("Your full name is : " + fullName);
+        
+        String sentence = "This is full name is : Minded Code ";
+        System.out.println(sentence);
+        
+        
+        //from user input 
+        System.out.println("\n\nYour name is : ");
+        String name1 = sc.next();
+        System.out.println(name1);
+        
+        System.out.println("Your full name is : ");
+        String fullName1 = sc.nextLine();
+        System.out.println(fullName1);
+        
+        // System.out.println("Enter your full sentence : ");
+        // String sentence1 = sc.nextLine();
+        // System.out.println(sentence1);
+        
+        
+    }
+}
+
+second input nhi le rhi hai full name vali string variable
